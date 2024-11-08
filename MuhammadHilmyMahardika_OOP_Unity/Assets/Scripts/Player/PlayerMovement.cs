b5fba4879f8206e9aff192cc2ed6e5ab9257af0e
hilmy194 <m.hilmy.mahardika@gmail.com>
2024-11-08 23:42:20 +0700

    CS modul 8

--- MuhammadHilmyMahardika_OOP_Unity/Assets/Scripts/Player/PlayerMovement.cs
+++ MuhammadHilmyMahardika_OOP_Unity/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,12 +6,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Vector2 timeToFullSpeed;
     [SerializeField] private Vector2 timeToStop;
     [SerializeField] private Vector2 stopClamp;
-    
     private Vector2 moveDirection;
     private Vector2 moveVelocity;
     private Vector2 moveFriction;
     private Vector2 stopFriction;
     private Rigidbody2D rb;
+
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
 
     private void Start()
     {
@@ -19,19 +21,24 @@ public class PlayerMovement : MonoBehaviour
         moveVelocity = 2 * maxSpeed / timeToFullSpeed;
         moveFriction = -2 * maxSpeed / (timeToFullSpeed * timeToFullSpeed);
         stopFriction = -2 * maxSpeed / (timeToStop * timeToStop);
-    }
 
-    private void FixedUpdate()
-    {
-        Move();
+        // Menentukan batas pergerakan dari kamera
+        Camera cam = Camera.main;
+        minBounds = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+        maxBounds = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
     }
 
     public void Move()
     {
-        float movingTheX = Input.GetAxis("Horizontal");
-        float movingTheY = Input.GetAxis("Vertical");
-        moveDirection = new Vector2(movingTheX, movingTheY).normalized;
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        moveDirection = new Vector2(inputX, inputY).normalized;
+
         rb.velocity = moveDirection * maxSpeed;
+
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x, maxBounds.x);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y, maxBounds.y);
+        transform.position = new Vector2(clampedX, clampedY);
     }
 
     private Vector2 GetFriction()
