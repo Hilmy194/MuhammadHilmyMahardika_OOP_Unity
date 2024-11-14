bd285a13ad55f9bfbb17b91337d8e48cd118a3ef
hilmy194 <m.hilmy.mahardika@gmail.com>
2024-11-14 23:55:09 +0700

    done

---
+++ MuhammadHilmyMahardika_OOP_Unity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Bullet Stats")]
+    public float bulletSpeed = 20f;
+    public int damage = 10;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        rb.velocity = transform.up * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy") || other.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
